1a0e887c2df6127d0dc1d01e8addcdbbf07cda06
༄༂Yuuki༉ <binhandoan9@gmail.com>
2023-02-15 22:56:07 +0700

    test22

---
+++ .history/test22_20230215171141.js
@@ -0,0 +1,11 @@
+// write a js function that returns a passed string with letters in alphabetical order.
+var sortStr = (str) =>
+{
+    var arr = str.split("");
+    arr.sort((a,b) => a > b ? 1: 0);
+    arr.join("");
+    return arr;
+} 
+
+    
+console.log(sortStr("hello"));
---
+++ .history/test22_20230215171316.js
@@ -0,0 +1,5 @@
+// write a js function that returns a passed string with letters in alphabetical order.
+var sortStr = (str) => str.split("").sort((a,b) => a> b ? 1 :-1).join("");
+
+    
+console.log(sortStr("webmaster"));
